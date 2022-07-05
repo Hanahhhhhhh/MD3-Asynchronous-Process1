a114fe572688375f0f29f3e2711de02dd1bbe047
hanahhhhhhhh <hanguyen18052004@gmail.com>
2022-07-05 14:12:18 +0700

    first commit

---
+++ Bai tap/timeCountDown/app.js
@@ -0,0 +1,23 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+rl.question("Input the time ", (timeInput) => {
+    let i = timeInput;
+    function startTimer() {
+        let countdownTimer = setInterval(function () {
+            console.log(i--);
+            if (i <= 0) {
+                console.log("counter finished")
+                clearTimeout(countdownTimer);
+            }
+        }, 1000);
+    }
+    startTimer();
+    rl.close()
+});
+
+
+
+
